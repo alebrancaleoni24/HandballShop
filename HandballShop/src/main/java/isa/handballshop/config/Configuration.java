93286217a8547655988d68ddc72322aae21ff50a
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2022-07-27 16:43:16 +0200

    Creata la classe Configuration per la configurazione dei percorsi e del jdbc

---
+++ HandballShop/src/main/java/isa/handballshop/config/Configuration.java
@@ -0,0 +1,30 @@
+package isa.handballshop.config;
+
+import java.util.Calendar;
+import java.util.logging.Level;
+import model.dao.JDBCDAOFactory;
+
+public class Configuration {
+    
+    /*DATABASE CONFIGURATION*/
+    /*Costante che mi permette di scegliere il DB da usare. Tale stringa è contenuta
+    nel DAOFactory corrispondente al DB che voglio usare*/
+    
+    public static final String DAO_IMPL=JDBCDAOFactory.DB2JDBCIMPL;
+
+    /*Scelta del Driver*/
+    public static final String MYSQL_DRIVER="com.mysql.jdbc.Driver";
+    public static final String DB2_DRIVER="com.ibm.db2.jcc";
+    /*Scelta Timezone*/
+    public static final String SERVER_TIMEZONE=Calendar.getInstance().getTimeZone().getID();
+    /*Configurazione URL database*/
+    public static final String MYSQL_URL="jdbc:mysql://localhost/hsDB?user=root&password=&serverTimezone="+SERVER_TIMEZONE;
+    public static final String DB2_URL="jdbc:db2://localhost:50000/testdb:user=DB2INST1;password=passw0rd";
+    
+    /*LOGGER FILES CONFIGURATION*/
+    public static final String LOG_NAME="HandallShop_log.%g.%u.txt";
+    public static final String DIR_LOG_WINDOWS="C:\\Users\\Riccardo\\Documents\\Progetto\\logs\\" + LOG_NAME;
+    public static final String DIR_LOG_LINUX="/home/alessandro/Documenti/HandballShop/HandballShop/Logs/" + LOG_NAME;
+    public static final Level GLOBAL_LOGGER_LEVEL=Level.ALL;
+    
+}
