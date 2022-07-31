3028a999cd8170877e35765d41f60f09f935d59a
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2022-07-31 15:03:33 +0200

    Creato il metodo per fare la view di pagamento .jsp

--- HandballShop/src/main/java/isa/handballshop/controller/Acquisto.java
+++ HandballShop/src/main/java/isa/handballshop/controller/Acquisto.java
@@ -81,6 +81,91 @@ public class Acquisto {
         }
     }
 
+    /* Metodo per portarmi dietro i cookie e fare la view di pagamento.jsp solo
+    se ho disponibilità di magazzino sufficiente per tutti i prodotti del carrello */
+    public static void ordina(HttpServletRequest request, HttpServletResponse response){
+        SessionDAO sessionDAO;
+        UtenteLoggato ul;
+        ArrayList<Carrello> carrelli = new ArrayList<Carrello>();
+        
+        JDBCDAOFactory jdbc = null;
+        
+        Logger logger = LogService.printLog();
+        try{
+            /*Creo la sessione*/
+            sessionDAO = new SessionDAOImpl();
+            sessionDAO.initSession(request, response);
+            
+            /*Recupero il cookie utente*/
+            UtenteLoggatoDAO ulDAO = sessionDAO.getUtenteLoggatoDAO();
+            ul = ulDAO.trova();
+            
+            /*Recupero il cookie carrello*/
+            CarrelloDAO carrelloDAO = sessionDAO.getCarrelloDAO();
+            carrelli = carrelloDAO.trova();
+            
+            /*Stabilisco la connessione*/
+            jdbc = JDBCDAOFactory.getJDBCImpl(Configuration.DAO_IMPL);
+            jdbc.beginTransaction();
+            
+            ArrayList<Prodotto> prodotti = new ArrayList<Prodotto>();
+            ProdottoDAO prodottoDAO = jdbc.getProdottoDAO();
+                        
+            /*Estraggo i prodotti dal DB*/
+            for(int i=0; i<carrelli.size(); i++){
+                prodotti.add(prodottoDAO.findByKey(carrelli.get(i).getCodiceProd()));
+            }
+            
+            /*CONTROLLO LA DISPONIBILITA' DI MAGAZZINO*/
+            TagliaDAO tagliaDAO = jdbc.getTagliaDAO();
+            boolean disponibilità = true;
+            
+            for(int i=0; i<carrelli.size(); i++){
+                if(carrelli.get(i).getQuantita() > tagliaDAO.getTagliaByKey(carrelli.get(i).getCodiceProd(), carrelli.get(i).getTaglia()).getQuantità() || prodotti.get(i).isBlocked()){
+                    disponibilità = false;
+                }
+            }
+            
+            if(!disponibilità){
+                /*
+                * Metodo comune a tutte le view di carrello.jsp
+                * Carico tutti i prodotti dal DB contenuti nel carrello e mappo le disponibilità
+                */
+                commonView(jdbc, sessionDAO, request);
+            }
+            
+            jdbc.commitTransaction();
+            
+            /*Setto gli attributi del viewModel*/
+            request.setAttribute("loggedOn",ul!=null);
+            request.setAttribute("loggedUser", ul);
+            if(disponibilità){
+                request.setAttribute("carrello", carrelli);
+                request.setAttribute("viewUrl", "acquisto/pagamento");
+            }else{
+                request.setAttribute("viewUrl", "acquisto/carrello");
+                request.setAttribute("applicationMessage", "Uno o più prodotti non sono disponibili per l'acquisto");
+            }
+            
+        }catch(Exception e){
+            logger.log(Level.SEVERE, "Errore Controller Acquisto", e);
+            try {
+                if(jdbc != null){
+                    jdbc.rollbackTransaction();
+                }
+            }catch(Throwable t){
+            }
+            throw new RuntimeException(e);
+        }finally{
+            try{
+                if(jdbc != null){
+                    jdbc.closeTransaction();
+                }
+            }catch(Throwable t){
+            }
+        }
+    }
+
     /*
     *
     * Metodo comune a tutte le view di carrello.jsp
