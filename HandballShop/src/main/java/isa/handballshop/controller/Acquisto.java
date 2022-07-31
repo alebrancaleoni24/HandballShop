2371de4bf686719bb889c5e94095c624d20bacde
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2022-07-31 15:51:47 +0200

    Creato il metodo paga nel controller Acquisto. Questo metodo serve per completare la procedura di acquisto inserendo i dati nel DB e fare la view di carrello.jsp

--- HandballShop/src/main/java/isa/handballshop/controller/Acquisto.java
+++ HandballShop/src/main/java/isa/handballshop/controller/Acquisto.java
@@ -3,21 +3,33 @@ package isa.handballshop.controller;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import isa.handballshop.config.Configuration;
+import isa.handballshop.model.dao.ContieneDAO;
 import isa.handballshop.model.dao.JDBCDAOFactory;
+import isa.handballshop.model.dao.OrdineDAO;
+import isa.handballshop.model.dao.PagamentoDAO;
 import isa.handballshop.model.dao.ProdottoDAO;
 import isa.handballshop.model.dao.TagliaDAO;
+import isa.handballshop.model.dao.UtenteDAO;
+import isa.handballshop.model.dao.exception.DuplicatedObjectException;
 import isa.handballshop.model.session.dao.CarrelloDAO;
 import isa.handballshop.model.session.dao.SessionDAO;
 import isa.handballshop.model.session.dao.UtenteLoggatoDAO;
 import isa.handballshop.model.session.dao.implementation.SessionDAOImpl;
 import isa.handballshop.model.session.valueObject.Carrello;
 import isa.handballshop.model.session.valueObject.UtenteLoggato;
+import isa.handballshop.model.valueObject.Contiene;
+import isa.handballshop.model.valueObject.Ordine;
+import isa.handballshop.model.valueObject.Pagamento;
 import isa.handballshop.model.valueObject.Prodotto;
+import isa.handballshop.model.valueObject.Taglia;
+import isa.handballshop.model.valueObject.Utente;
 import isa.handballshop.services.logservice.LogService;
 
 public class Acquisto {
@@ -233,6 +245,165 @@ public class Acquisto {
             request.setAttribute("carrello", carrelli);
             
             
+        }catch(Exception e){
+            logger.log(Level.SEVERE, "Errore Controller Acquisto", e);
+            try {
+                if(jdbc != null){
+                    jdbc.rollbackTransaction();
+                }
+            }catch(Throwable t){
+            }
+            throw new RuntimeException(e);
+        }finally{
+            try{
+                if(jdbc != null){
+                    jdbc.closeTransaction();
+                }
+            }catch(Throwable t){
+            }
+        }
+    }
+
+    /* Metodo per il pagamento del carrello, view di carrello.jsp */
+    public static void paga(HttpServletRequest request, HttpServletResponse response){
+        SessionDAO sessionDAO;
+        UtenteLoggato ul;
+        ArrayList<Carrello> carrelli = new ArrayList<Carrello>();
+        String applicationMessage = null;
+        
+        JDBCDAOFactory jdbc = null;
+        
+        Logger logger = LogService.printLog();
+        try{
+            /*Creo la sessione*/
+            sessionDAO = new SessionDAOImpl();
+            sessionDAO.initSession(request, response);
+            
+            /*Recupero il cookie utente*/
+            UtenteLoggatoDAO ulDAO = sessionDAO.getUtenteLoggatoDAO();
+            ul = ulDAO.trova();
+            
+            /*Recupero il cookie carrello*/
+            CarrelloDAO carrelloDAO = sessionDAO.getCarrelloDAO();
+            carrelli = carrelloDAO.trova();
+            
+            /*Stabilisco la connessione*/
+            jdbc = JDBCDAOFactory.getJDBCImpl(Configuration.DAO_IMPL);
+            jdbc.beginTransaction();
+            
+            boolean disponibilità = true;
+            
+            TagliaDAO tagliaDAO = jdbc.getTagliaDAO();
+            Taglia taglia = null;
+            
+            /*Estraggo i prodotti dal DB*/
+            ArrayList<Prodotto> prodotti = new ArrayList<Prodotto>();
+            ProdottoDAO prodottoDAO = jdbc.getProdottoDAO();
+            
+            for(int i=0; i<carrelli.size(); i++){
+                prodotti.add(prodottoDAO.findByKey(carrelli.get(i).getCodiceProd()));
+            }
+            
+            /*CONTROLLO DISPONIBILITA*/
+            for(int j=0; j<carrelli.size(); j++){
+                if(carrelli.get(j).getQuantita() > tagliaDAO.getTagliaByKey(carrelli.get(j).getCodiceProd(), carrelli.get(j).getTaglia()).getQuantità() || prodotti.get(j).isBlocked()){
+                    disponibilità = false;
+                }
+            }
+            
+            /* Se nel frattempo è rimasta disponibilità procedo all'acquisto, altrimenti mando un messaggio all'utente */
+            if(disponibilità){
+            
+                /*Carico l'utente loggato*/
+                UtenteDAO utenteDAO = jdbc.getUtenteDAO();
+                Utente utente = utenteDAO.findByEmail(ul.getEmail());
+            
+                Pagamento pagamento = null;
+                PagamentoDAO pagamentoDAO = jdbc.getPagamentoDAO();
+            
+                /*****INIZIO TRANSAZIONE*****/
+                Date dataOdierna = new Date();
+                
+                /*INSERIMENTO PAGAMENTO NEL DB*/
+                try{
+                    pagamento = pagamentoDAO.creaPagamento("confermato", 
+                            request.getParameter("carta"), 
+                            dataOdierna, dataOdierna, Float.parseFloat(request.getParameter("prezzo")),
+                            utente, null);
+                }catch(DuplicatedObjectException doe){
+                    applicationMessage = "Pagamento già esistente";
+                    logger.log(Level.INFO, "Tentativo di inserimento di un pagamento già esistente");
+                }
+            
+                Ordine ordine = null;
+                OrdineDAO ordineDAO = jdbc.getOrdineDAO();
+            
+                /* Imposto una data di consegna di default 3 giorni successivi alla data dell'ordine */
+                java.util.Date dataConsegna = new Date();
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(dataConsegna);
+                cal.add(Calendar.DATE, 3);
+                dataConsegna = cal.getTime();
+            
+                /*INSERIMENTO ORDINE NEL DB*/
+                try{
+                    ordine = ordineDAO.creaOrdine(dataOdierna, "In preparazione", dataConsegna, request.getParameter("nazione"), 
+                            request.getParameter("citta"), request.getParameter("via"), request.getParameter("numeroCivico"), 
+                            Integer.parseInt(request.getParameter("CAP")), pagamento, utente, null);
+                }catch(DuplicatedObjectException doe){
+                    applicationMessage = "Ordine già esistente";
+                    logger.log(Level.INFO, "Tentativo di inserimento di un ordine già esistente");
+                }
+            
+                Contiene contiene = null;
+                ContieneDAO contieneDAO = jdbc.getContieneDAO();
+            
+                /*INSERIMENTO CONTIENE NEL DB*/
+                for(int i=0; i<carrelli.size(); i++){
+                    try{
+                        contiene = contieneDAO.creaContiene(ordine.getCodiceOrdine(), carrelli.get(i).getCodiceProd(), 
+                            carrelli.get(i).getQuantita(), carrelli.get(i).getTaglia());
+                    }catch(DuplicatedObjectException doe){
+                        applicationMessage = "Contiene già esistente";
+                        logger.log(Level.INFO, "Tentativo di inserimento di un 'Contiene' già esistente");
+                    }
+                }
+            
+                /*MODIFICA GIACENZA NEL DB*/
+                for(int i=0; i<carrelli.size(); i++){
+                    taglia = tagliaDAO.getTagliaByKey(carrelli.get(i).getCodiceProd(), carrelli.get(i).getTaglia());
+                    taglia.setQuantità(taglia.getQuantità()-carrelli.get(i).getQuantita());
+                    tagliaDAO.aggiorna(taglia);
+                }
+            
+                /*CANCELLA COOCKIE CARRELLO*/
+                carrelloDAO.elimina();
+                applicationMessage = "Ordine avvenuto con successo";
+                
+                /*
+                * Dopo la transazione ritorno alla pagina del carrello che, anche se vuoto, si aspetta di ricevere un parametro chiamato prezzo e
+                * quindi gliene passo uno fittizio
+                */
+                double prezzo = 0;
+                request.setAttribute("prezzo", prezzo);
+            }else{
+                /* Caso in cui sia cambiata la disponibilità di magazzino */
+                applicationMessage = "Disponibilità modificata durante la transazione, impossibile procedere all'ordine";
+                
+                /*
+                *
+                * Metodo comune a tutte le view di carrello.jsp
+                * Carico tutti i prodotti dal DB contenuti nel carrello e mappo le disponibilità
+                */
+                commonView(jdbc, sessionDAO, request);
+            }
+              
+            jdbc.commitTransaction();
+            
+            request.setAttribute("loggedOn",ul!=null);
+            request.setAttribute("loggedUser", ul);
+            request.setAttribute("applicationMessage", applicationMessage);
+            request.setAttribute("viewUrl", "acquisto/carrello");
         }catch(Exception e){
             logger.log(Level.SEVERE, "Errore Controller Acquisto", e);
             try {
