3751c642fe9523a6e09248a7a1a7f7dbd582e2c3
Alessandro Brancaleoni <alessan.brancaleoni@edu.unife.it>
2022-08-02 11:05:03 +0200

    Creato il controller ProdottoManagement per la gestione dei prodotti

---
+++ HandballShop/src/main/java/isa/handballshop/controller/ProdottoManagement.java
@@ -0,0 +1,90 @@
+package isa.handballshop.controller;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import isa.handballshop.config.Configuration;
+import isa.handballshop.model.dao.JDBCDAOFactory;
+import isa.handballshop.model.dao.ProdottoDAO;
+import isa.handballshop.model.session.dao.SessionDAO;
+import isa.handballshop.model.session.dao.UtenteLoggatoDAO;
+import isa.handballshop.model.session.dao.implementation.SessionDAOImpl;
+import isa.handballshop.model.session.valueObject.UtenteLoggato;
+import isa.handballshop.model.valueObject.Prodotto;
+import isa.handballshop.services.logservice.LogService;
+
+public class ProdottoManagement {
+    
+    /*
+    * Classe per visualizzare i prodotti presenti in magazzino e gestirli
+    * Usata per magazzino.jsp, gestisciProdotto.jsp e gestisciTaglie.jsp
+    */
+    
+    /* Metodo per eseguire la view di magazzino.jsp */
+    public static void view(HttpServletRequest request, HttpServletResponse response){
+        SessionDAO sessionDAO;
+        UtenteLoggato ul;
+        
+        JDBCDAOFactory jdbc = null;
+        
+        Logger logger = LogService.printLog();
+
+        try {
+            /*Creo la sessione*/
+            sessionDAO = new SessionDAOImpl();
+            sessionDAO.initSession(request, response);
+            
+            /*Recupero il cookie utente*/
+            UtenteLoggatoDAO ulDAO = sessionDAO.getUtenteLoggatoDAO();
+            ul = ulDAO.trova();
+            
+            /*Stabilisco la connessione*/
+            jdbc = JDBCDAOFactory.getJDBCImpl(Configuration.DAO_IMPL);
+            jdbc.beginTransaction();
+
+            /*Chiamo il metodo uguale a tutte le chiamate per caricare
+            tutti i prodotti nel DB*/
+            commonView(jdbc, sessionDAO, request);
+
+            jdbc.commitTransaction();
+
+            request.setAttribute("loggedOn",ul!=null);
+            request.setAttribute("loggedUser", ul);
+            request.setAttribute("viewUrl", "prodottoManagement/magazzino");
+
+        }catch(Exception e){
+            logger.log(Level.SEVERE, "Errore Controller prodottoManagement", e);
+            try {
+                if(jdbc != null){
+                    jdbc.rollbackTransaction();
+                }
+            }catch(Throwable t){
+            }
+            throw new RuntimeException(e);
+        }finally{
+            try{
+                if(jdbc != null){
+                    jdbc.closeTransaction();
+                }
+            }catch(Throwable t){
+            }
+        }
+    }
+
+    /* Metodo uguale a tutte le chiamate di magazzino.jsp per caricare tutti i prodotti nel DB */
+    private static void commonView(JDBCDAOFactory jdbc, SessionDAO sessionDAO, HttpServletRequest request) {
+        ArrayList<Prodotto> prodotti;
+
+        ProdottoDAO prodottoDAO = jdbc.getProdottoDAO();
+
+        prodotti = prodottoDAO.trovaProdotti();
+
+        /* Setto gli attributi del view model */
+        request.setAttribute("prodotti", prodotti);
+
+    }
+}
